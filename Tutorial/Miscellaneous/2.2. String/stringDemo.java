7e630248b64680a9cc1c3579b1159c5f3212690b
Md Fokhrul Alam <55650168+FokhrulAlam@users.noreply.github.com>
2019-11-27 18:16:43 -0600

    Add files via upload

---
+++ Tutorial/Miscellaneous/2.2. String/stringDemo.java
@@ -0,0 +1,11 @@
+public class stringDemo {
+    public static void main(String[] args){
+        String myString = "Hi there! This is Fokhrul.";
+        int stringLength = myString.length();   //length() is a function
+
+        System.out.println(myString);
+        System.out.println("The length of the string is "+stringLength);
+
+        System.out.println("The string in lower case is: "+myString.toLowerCase());     //String in lower case
+    }
+}
