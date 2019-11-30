b41add11bfce0850de4e00de508f53a3ffc8d4ef
Md Fokhrul Alam <55650168+FokhrulAlam@users.noreply.github.com>
2019-11-29 18:22:09 -0600

    Add files via upload

---
+++ Tutorial/Exercises/Miscellaneous/1.3. GMH-HW/1.1. HW 1/HwOne1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HwOne1 {
+    public static void main(String[] args){
+        Scanner math = new Scanner(System.in);
+
+        System.out.println("Please enter the first number: ");
+        int number1 = math.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int number2 = math.nextInt();
+
+        System.out.println("Please enter either + or - or * or / : ");
+        String operator = math.next();
+
+        switch (operator){
+            case "+":
+                System.out.println(number1+operator+number2+"="+(number1+number2));
+                break;
+            case "-":
+                System.out.println(number1+operator+number2+"="+(number1-number2));
+                break;
+            case "*":
+                System.out.println(number1+operator+number2+"="+(number1*number2));
+                break;
+            case "/":
+                System.out.println(number1+operator+number2+"="+(number1/number2));
+                break;
+        }
+
+    }
+}
