69b78517e27974b6c3321e9e5aef77cff295e04c
Md Fokhrul Alam <55650168+FokhrulAlam@users.noreply.github.com>
2019-12-01 21:53:17 -0600

    Add files via upload

---
+++ Tutorial/Exercises/Miscellaneous/1.5. Book of Tony Gadis/1.1. Personal Informatin/PersonalInformation.java
@@ -0,0 +1,14 @@
+
+public class PersonalInformation {
+	// defining variables
+	public static void main(String ...args) {
+		String name = "Md Fokhrul Alam";
+		String address = "345 Alabama St, Beaumont,TX-77708";
+		String telephoneNumber = "90564782";
+		String collegeMajor = "Electrical Engineering";
+		
+		// display personal information
+		
+		System.out.println(name+"\n"+address+"\n"+telephoneNumber+"\n"+collegeMajor);
+	}
+}
