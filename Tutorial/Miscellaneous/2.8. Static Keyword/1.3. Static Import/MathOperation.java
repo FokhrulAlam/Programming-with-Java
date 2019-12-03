0124bb39fbbd393fad1e15c18db805a38efaba56
Md Fokhrul Alam <55650168+FokhrulAlam@users.noreply.github.com>
2019-12-02 22:01:05 -0600

    Add files via upload

---
+++ Tutorial/Miscellaneous/2.8. Static Keyword/1.3. Static Import/MathOperation.java
@@ -0,0 +1,10 @@
+package practice.importingpackage;
+
+public class MathOperation {
+	public static int main(String[] args) {
+		int num1 = UserInput.a;
+		int num2 = UserInput.b;
+		return num1*num2;
+	}
+
+}
---
+++ Tutorial/Miscellaneous/2.8. Static Keyword/1.3. Static Import/UserInput.java
@@ -0,0 +1,8 @@
+package practice.importingpackage;
+import java.util.Scanner;
+
+public class UserInput {
+	static int a = 10;
+	static int b = 11;
+
+}
